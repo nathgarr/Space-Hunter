1135e446a656ad4a3204acb7878df76fba3b4e9e
Harous <89517296+nathgarr@users.noreply.github.com>
2024-11-26 12:53:23 +0100

    fix + new visual

--- SpaceHunterProject/Assets/Script/EndScenePlayerController.cs
+++ SpaceHunterProject/Assets/Script/EndScenePlayerController.cs
@@ -7,10 +7,11 @@ public class EndScenePlayerController : MonoBehaviour
     [SerializeField]
     public Transform[] particlePos;
     [SerializeField]
-    public Transform player;
+    public Rigidbody rb;
     [SerializeField]
     public ParticleSystem  verticalEmitters, horizontalEmitters;
     float speed = 2;
+    float verticalAxis, horizontalAxis;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,32 +21,35 @@ public class EndScenePlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float verticalAxis = Input.GetAxis("Vertical");
+        verticalAxis = Input.GetAxis("Vertical");
         if (verticalAxis != 0) { verticalEmitters.Play();  } 
         else { verticalEmitters.Stop(); }
-        UpdateEmittersPos(verticalAxis, 0);
-        float horizontalAxis = Input.GetAxis("Horizontal");
+         horizontalAxis = Input.GetAxis("Horizontal");
         if (horizontalAxis != 0) { horizontalEmitters.Play(); } 
         else { horizontalEmitters.Stop(); }
-        UpdateEmittersPos(0, horizontalAxis);
+        UpdateEmittersPos(verticalAxis, horizontalAxis);
+    }
+    public void FixedUpdate()
+    {
         PlayerMove(speed);
-
     }
 
     public void UpdateEmittersPos(float verticalAxis , float horizonralAxis)
     {
-        int index = verticalAxis < 0 ? 0: 1;
+        int index = verticalAxis < 0f ? 0: 1;
         verticalEmitters.transform.position = particlePos[index].position;
         verticalEmitters.transform.rotation = particlePos[index].rotation;
-        index = horizonralAxis < 0 ? 2: 3;
+        Debug.Log(index);
+        index = horizonralAxis < 0f ? 2: 3;
         horizontalEmitters.transform.position= particlePos[index].position;
         horizontalEmitters.transform.rotation= particlePos[index].rotation;
+       
     }
     public void PlayerMove( float speed)
     {
         float verticalAxis = Input.GetAxis("Vertical");
         float horizontalAxis = Input.GetAxis("Horizontal");
-      /*  player.transform.position.x = horizontalAxis * speed;
-        player.transform.position.y = verticalAxis * speed;*/
+        Vector3 velocity = new Vector3(horizontalAxis, verticalAxis, 0);
+        rb.AddForce(velocity);
     }
 }
